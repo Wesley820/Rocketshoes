d07d6c43fb784aa819eb8ae750e1d85946c88fb3
Wesley Oliveira <assiswesley549@gmail.com>
2020-08-23 06:47:34 -0300

    home page styled

---
+++ src/pages/Home/index.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
+
+function Home() {
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/camisa-de-compressao-termica-stigli-pro-protecao-solar-fpu-50-manga-longa-rash-guard/06/826-5613-006/826-5613-006_zoom1.jpg?ts=1587507334&"
+          alt="product"
+        />
+        <strong>Camisa térmica (compressão)</strong>
+        <span>R$100,00</span>
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/camisa-de-compressao-termica-stigli-pro-protecao-solar-fpu-50-manga-longa-rash-guard/06/826-5613-006/826-5613-006_zoom1.jpg?ts=1587507334&"
+          alt="product"
+        />
+        <strong>Camisa térmica (compressão)</strong>
+        <span>R$100,00</span>
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/camisa-de-compressao-termica-stigli-pro-protecao-solar-fpu-50-manga-longa-rash-guard/06/826-5613-006/826-5613-006_zoom1.jpg?ts=1587507334&"
+          alt="product"
+        />
+        <strong>Camisa térmica (compressão)</strong>
+        <span>R$100,00</span>
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/camisa-de-compressao-termica-stigli-pro-protecao-solar-fpu-50-manga-longa-rash-guard/06/826-5613-006/826-5613-006_zoom1.jpg?ts=1587507334&"
+          alt="product"
+        />
+        <strong>Camisa térmica (compressão)</strong>
+        <span>R$100,00</span>
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/camisa-de-compressao-termica-stigli-pro-protecao-solar-fpu-50-manga-longa-rash-guard/06/826-5613-006/826-5613-006_zoom1.jpg?ts=1587507334&"
+          alt="product"
+        />
+        <strong>Camisa térmica (compressão)</strong>
+        <span>R$100,00</span>
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/camisa-de-compressao-termica-stigli-pro-protecao-solar-fpu-50-manga-longa-rash-guard/06/826-5613-006/826-5613-006_zoom1.jpg?ts=1587507334&"
+          alt="product"
+        />
+        <strong>Camisa térmica (compressão)</strong>
+        <span>R$100,00</span>
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+    </ProductList>
+  );
+}
+
+export default Home;
